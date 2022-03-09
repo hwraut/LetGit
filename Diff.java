13e1f359a5d5ac9a9d2079c828f79b1740af91b0
Himanshu Raut <hwraut99@gmail.com>
2022-03-09 09:30:18 +0530

    Diff commit3

--- Diff.java
+++ Diff.java
@@ -3,7 +3,7 @@ public class Diff {
     public static void main(String[] args)
     {
         int c=5-2;
-        System.out.println("Diff = "+ c);
+        System.out.println("Difference between 5 and 2 is : "+ c);
     }
     
 }
